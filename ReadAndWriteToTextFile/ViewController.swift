08f21091c1e3568e04d31d3efbc569b054fac8b0
Spur IQ <spuriq@rishs-macbook-air.local>
2019-05-26 13:35:10 -0700

    Initial Commit

---
+++ ReadAndWriteToTextFile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadAndWriteToTextFile
+//
+//  Created by Spur IQ on 5/26/19.
+//  Copyright © 2019 Praveena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
