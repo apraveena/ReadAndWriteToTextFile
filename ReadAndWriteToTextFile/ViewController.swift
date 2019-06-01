e10f713c90582e1e33b156cd2d262a7c8169c027
Spur IQ <spuriq@rishs-macbook-air.local>
2019-06-01 11:55:00 -0700

    Text file functionality is completed Delete functionality is implemented.

--- ReadAndWriteToTextFile/ViewController.swift
+++ ReadAndWriteToTextFile/ViewController.swift
@@ -8,13 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var allToys = ["Basket Ball", "Rubik's cube", "Darts", "Exploding Kittens", "Breakable Erasers"]
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var myTableView: UITableView!
+    //let fileName = "toys"
+    //let fileExt = "txt"
+    let strFileName = "spurIQArrayTest.txt"
+    //let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allToys.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellID")
+        cell.textLabel?.text = allToys[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if(editingStyle == UITableViewCell.EditingStyle.delete){
+            allToys.remove(at: indexPath.row)
+            writeArrayToFile()
+            myTableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        readDataFromFile()
+        myTableView.reloadData()
+    }
+    
+    @IBAction func SaveToArrayButton(_ sender: Any) {
+        if(input.text != "") {
+            allToys.append(input.text!)
+            writeArrayToFile()
+            myTableView.reloadData()
+            input.text = ""
+        }
+    }
+    
+    func readDataFromFile(){
+        let fileURL = dir?.appendingPathComponent(strFileName)
+        allToys = NSMutableArray(contentsOf: fileURL!) as! [String]
+    }
+    
+    // This is to write array of data to a file
+    func writeArrayToFile()
+    {
+       // let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+     let fileURL = dir?.appendingPathComponent(strFileName)
+       (allToys as NSArray).write(to: fileURL!, atomically: true)
+    }
+    
 }
 
